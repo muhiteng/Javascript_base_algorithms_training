10838abb5b84689eb3323811e407ae7b458b2698
unknown <muh.it.eng.19841984@gmail.com>
2022-12-29 16:51:48 +0200

    recursive Fibonacci

---
+++ math/fibonacci_recursive.js
@@ -0,0 +1,14 @@
+
+// time complexity is Big-O = O(2^n)
+// not good for fibonacci
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
+}
+
+console.log(recursiveFibonacci(0)) // 0
+console.log(recursiveFibonacci(1)) // 1
+console.log(recursiveFibonacci(6)) // 8
+console.log(recursiveFibonacci(7)) // 13
