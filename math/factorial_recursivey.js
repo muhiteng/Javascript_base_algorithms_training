ff413eaf2dbec89a74defd5226259d55fcfa26cc
unknown <muh.it.eng.19841984@gmail.com>
2022-12-29 16:57:10 +0200

    recursive Factorial

---
+++ math/factorial_recursivey.js
@@ -0,0 +1,11 @@
+// time complexity is Big-O = O(n)
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1
+  }
+  return n * recursiveFactorial(n - 1)
+}
+
+console.log(recursiveFactorial(0)) // 1
+console.log(recursiveFactorial(1)) // 1
+console.log(recursiveFactorial(5)) // 120
