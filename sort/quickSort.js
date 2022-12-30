cfc112063af17e057adc866bfc393a4f0d3923e8
unknown <muh.it.eng.19841984@gmail.com>
2022-12-30 09:03:07 +0200

    quick sort

---
+++ sort/quickSort.js
@@ -0,0 +1,21 @@
+
+// worst case time complexity is Big-O = O(n^2)
+// Avg case is O(nlogn)
+function quickSort(arr) {
+    if (arr.length < 2) return arr
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i])
+      } else {
+        right.push(arr[i])
+      }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+  }
+  
+  const arr = [8, 20, -2, 4, -6]
+  console.log(quickSort(arr)) // [-6, -2, 4, 8, 20]
+  
